864ec228089240f66401a96f889a53c05fd66165
mascotitasfelices <mascotitasfelices2@gmail.com>
2026-09-08 07:17:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,13 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// Instalación del Service Worker y guardado en caché
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-// Activación del Service Worker
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
 });
 
-// Evento Fetch obligatorio para cumplir los requisitos de PWA
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => new Response('Conexión requerida'))
   );
 });
